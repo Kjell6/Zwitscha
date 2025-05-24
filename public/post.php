ab2a652e698aa682f2c76b1f5c58ba0dbaac08e4
Kjell6 <143009706+Kjell6@users.noreply.github.com>
2025-05-24 13:15:38 +0200

    man kann jetzt auf einen freien bereich im post klicken, um die details zu öffnen

--- public/post.php
+++ public/post.php
@@ -1,4 +1,4 @@
-<article class="post">
+<article class="post" onclick="window.location.href = 'postDetails.php';">
         <a href="Profil.php" class="no-post-details">
             <img
                     src="<?= htmlspecialchars($post['profilBild']) ?>"
@@ -16,9 +16,9 @@
                 </a>
 
                 <div class="post-user-details">
-                    <span class="post-author-name">
+                    <a href="Profil.php" class="post-author-name">
                         <?= htmlspecialchars($post['autor']) ?>
-                    </span>
+                    </a>
                     <time
                             datetime="<?= htmlspecialchars($post['datumZeit']) ?>"
                             class="post-timestamp">
@@ -29,7 +29,8 @@
                     <button
                             class="post-options-button no-post-details"
                             type="button"
-                            aria-label="Post-Optionen">
+                            aria-label="Post-Optionen"
+                            onclick="event.stopPropagation();">
                         <i class="bi bi-trash-fill"></i>
                     </button>
                 <?php endif; ?>
@@ -46,7 +47,7 @@
                                 src="<?= htmlspecialchars($post['bildPfad']) ?>"
                                 alt="Post-Bild"
                                 class="post-image"
-                                onclick="openLightbox('<?= htmlspecialchars($post['bildPfad']) ?>')"
+                                onclick="openLightbox('<?= htmlspecialchars($post['bildPfad']) ?>'); event.stopPropagation()"
                                 style="cursor: pointer;">
                     </div>
                 <?php endif; ?>
@@ -58,7 +59,8 @@
                         <button
                                 class="reaction-button no-post-details"
                                 type="button"
-                                data-emoji="<?= htmlspecialchars($emoji) ?>">
+                                data-emoji="<?= htmlspecialchars($emoji) ?>"
+                                onclick="event.stopPropagation();">
                             <?= $emoji ?>
                             <span class="reaction-counter"><?= (int)$count ?></span>
                         </button>
@@ -66,7 +68,7 @@
                 </div>
 
                 <a href="postDetails.php" class="comment-link">
-                    <button class="action-button comment-button" type="button">
+                    <button class="action-button comment-button" type="button" onclick="event.stopPropagation();">
                         <i class="bi bi-chat-dots-fill"></i>
                         <?= (int)$post['comments'] ?> Kommentar<?= $post['comments'] !== 1 ? 'e' : '' ?>
                     </button>
