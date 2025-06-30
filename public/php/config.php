052d2e969d3be6c688e1e75b44e79501b85aece8
Kjell6 <143009706+Kjell6@users.noreply.github.com>
2025-06-30 19:44:50 +0200

    Zeitzone wird bei Config korrekt gesetzt

--- public/php/config.php
+++ public/php/config.php
@@ -1,6 +1,10 @@
 <?php
 // public/php/config.php
 
+// Setzt die Standardzeitzone für die gesamte Anwendung auf Mitteleuropäische Zeit.
+// Dies stellt sicher, dass alle Zeitstempel korrekt umgerechnet werden.
+date_default_timezone_set('Europe/Berlin');
+
 return [
     'host' => getenv('MYSQL_SERVER'),
     'user' => getenv('MYSQL_USER'),
